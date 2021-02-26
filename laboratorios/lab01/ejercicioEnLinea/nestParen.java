b8397840fa5f8e65be0b93bca53aeb7a33a87dd6
Brigith-giraldo <78159254+Brigith-giraldo@users.noreply.github.com>
2021-02-26 18:54:34 -0500

    Update nestParen.java

--- laboratorios/lab01/ejercicioEnLinea/nestParen.java
+++ laboratorios/lab01/ejercicioEnLinea/nestParen.java
@@ -1,1 +1,15 @@
+public class nestParen{
+  
+    public boolean nestParen(String str) {
 
+        if(str.length() <= 1) return true;
+
+        if(str.length()%2 == 0){
+            if(str.charAt(0) == '(' && str.charAt(str.length() - 1) == ')')
+                return nestParen(str.substring(1,str.length()-1));
+            return false;
+        }
+        return false;
+    }
+
+}
