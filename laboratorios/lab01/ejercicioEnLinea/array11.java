baef4c9ce8378ea5e7bb8d05db3705e24090ae91
Brigith-giraldo <78159254+Brigith-giraldo@users.noreply.github.com>
2021-02-26 18:58:30 -0500

    Update array11.java

--- laboratorios/lab01/ejercicioEnLinea/array11.java
+++ laboratorios/lab01/ejercicioEnLinea/array11.java
@@ -1,1 +1,10 @@
-
+public class array11{
+  
+    public int array11(int[] nums, int index) {
+        if(index<nums.length){
+            if(nums[index] == 11) return 1 + array11(nums,index+1);
+            return array11(nums,index+1);
+        }
+        return 0;
+    }
+}
