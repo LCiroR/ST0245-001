5822f41afa4fc4c969333e2435164c81eb43e370
Brigith-giraldo <78159254+Brigith-giraldo@users.noreply.github.com>
2021-02-26 19:11:14 -0500

    Update count11.java

--- laboratorios/lab01/ejercicioEnLinea/count11.java
+++ laboratorios/lab01/ejercicioEnLinea/count11.java
@@ -1,3 +1,15 @@
 /**
 * Given a string, compute recursively (no loops) the number of "11" substrings in the string
 */
+public class count11{
+  
+    public int count11(String str) {
+        if(str.length() > 1){
+            if(str.substring(0,2).equals("11"))
+                return 1 + count11(str.substring(2));
+            return count11(str.substring(1));
+        }
+
+        return 0;
+    }
+}
