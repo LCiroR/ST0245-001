6412281aa6ad9b1132d70273fd1edd12862176a4
Brigith-giraldo <78159254+Brigith-giraldo@users.noreply.github.com>
2021-02-26 19:04:19 -0500

    Update groupNoAdj.java

--- laboratorios/lab01/ejercicioEnLinea/groupNoAdj.java
+++ laboratorios/lab01/ejercicioEnLinea/groupNoAdj.java
@@ -1,1 +1,10 @@
-
+public class groupNoAdj{
+  
+    public boolean groupNoAdj(int start, int[] nums, int target) {
+        if(start >= nums.length)
+            return target == 0;
+        else
+            return (groupNoAdj(start + 2, nums, target - nums[start]) ||
+                    groupNoAdj(start + 1, nums, target));
+    }
+}
