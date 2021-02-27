66cbb1d7178f202fa4acece7b61e824a8c8d2783
LCiroR <67881143+LCiroR@users.noreply.github.com>
2021-02-26 19:21:28 -0500

    Update splitOdd10.java

--- laboratorios/lab01/ejercicioEnLinea/splitOdd10.java
+++ laboratorios/lab01/ejercicioEnLinea/splitOdd10.java
@@ -1,3 +1,7 @@
+/**
+* Given an array of ints, is it possible to divide the ints into two groups, so that the sum of one group is a multiple of 10, 
+* and the sum of the other group is odd. Every int must be in one group or the other.
+*/
 public class splitOdd10{
   
     public boolean splitOdd10(int[] nums) {
