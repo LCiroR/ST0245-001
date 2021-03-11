7557381e3066c59d78fe46b4bc37670eb8067e0e
LCiroR <67881143+LCiroR@users.noreply.github.com>
2021-03-11 17:32:57 -0500

    Create sum67.java

---
+++ laboratorios/lab02/ejercicioEnLinea/sum67.java
@@ -0,0 +1,20 @@
+/**
+* Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 
+**/
+public int sum67(int[] nums) {
+    int sum = 0;
+    boolean inRange = false;
+      
+    for(int i = 0; i < nums.length; i++) {
+        if(nums[i] == 6){
+          inRange = true;
+        }
+        if(!inRange){
+          sum += nums[i];
+        }
+        if(inRange && nums[i] == 7){
+          inRange = false;
+        }
+    }
+    return sum;
+  }
