11b8249a2839987dd260cf6e7fcd4a85b74b6749
LCiroR <67881143+LCiroR@users.noreply.github.com>
2021-03-11 15:39:08 -0500

    Create countEvens.java

---
+++ laboratorios/lab02/ejercicioEnLinea/countEvens.java
@@ -0,0 +1,13 @@
+/** 
+* Return the number of even ints in the given array
+*/
+public int countEvens(int[] nums) {
+  int sum = 0;
+  for(int i = 0; i < nums.length; i++){
+    if(nums[i] % 2 == 0){
+      sum += 1;
+    }
+    continue;
+  }
+  return sum;
+}
