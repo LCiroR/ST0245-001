eef404d8735197577897101f7bf01e1d3a3b934d
Brigith-giraldo <78159254+Brigith-giraldo@users.noreply.github.com>
2021-03-28 16:04:28 -0500

    Create Triplet.java

---
+++ laboratorios/lab03/codigo/Triplet.java
@@ -0,0 +1,17 @@
+/**
+ * La clase Triplet es una tupla con 3 elementos
+ * Es como una pareja pero de 3 elementos.
+ *
+ * @author (Brigith giraldo, Luisa ciro)
+ * @version 1
+ */
+public class Triplet<X, Y, Z> { 
+  public final X x; 
+  public final Y y; 
+  public final Z z;
+  public Triplet(X x, Y y, Z z) { 
+    this.x = x; 
+    this.y = y; 
+    this.z = z;
+  } 
+} 
